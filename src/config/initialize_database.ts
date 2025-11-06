925016f87311ee720de08cdb3126755dae75028c
Vimal290704 <vimalyadavkr001@gmail.com>
2025-11-06 20:55:06 +0530

    prog: initializing backend services

---
+++ src/config/initialize_database.ts
@@ -0,0 +1,56 @@
+import pool from "./database";
+
+export const createTables = async () => {
+  const client = await pool.connect();
+
+  try {
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        username VARCHAR(100) UNIQUE NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS projects (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+        project_status VARCHAR(100) NOT NULL DEFAULT 'NOT_STARTED',
+        authToken TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS project_details (
+        id SERIAL PRIMARY KEY,
+        project_id INTEGER REFERENCES projects(id) ON DELETE CASCADE,
+        rules_md TEXT,
+        openapi_file JSONB,
+        schema JSONB,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS project_members (
+        id SERIAL PRIMARY KEY,
+        project_id INTEGER REFERENCES projects(id) ON DELETE CASCADE,
+        user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+        role VARCHAR(100),
+        joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    console.log("✅ All tables created successfully");
+  } catch (error: any) {
+    console.error("❌ Error creating tables:", error.message);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-import express from "express";
+import dotenv from "dotenv";
+import express, { type Request, type Response } from "express";
+import pool from "./config/database";
+import { createTables } from "./config/initialize_database";
+dotenv.config();
 
 const app: express.Application = express();
+
+const port: number = Number(process.env.PORT);
+
+app.get("/", async (req: Request, res: Response) => {
+  return res.send("API HIT");
+});
+
+app.listen(port, async () => {
+  try {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("Database connection error:", err.stack);
+      } else {
+        console.log("✅ Database connected successfully");
+        release();
+      }
+    });
+
+    await createTables();
+    console.log(`Server is running on ${port}`);
+  } catch (error: any) {
+    console.error("Error creating database: ", error);
+    process.exit(1);
+  }
+});
